e88979ca0e24633f1423ca858c7de631e8265235
root <root@localhost.localdomain>
2020-03-26 22:50:33 +0000

    parcel update

--- src/php/parcel.php
+++ src/php/parcel.php
@@ -6,7 +6,7 @@ function parcel_assets_path($filename = '', $public = false)
   return $base . '/' . (WP_DEBUG ? 'dev' : 'dist') . '/' . $filename;
 }
 
-function parcel_enqueue_resource($name, ...$args)
+function parcel_enqueue_resource($name, $deps = [], $ver = null, $footer = true)
 {
   $jsonFileName = parcel_assets_path('parcel.json');
   if (!is_readable($jsonFileName)) {
@@ -16,19 +16,29 @@ function parcel_enqueue_resource($name, ...$args)
   if (!property_exists($assets, $name)) return;
   $uri = parcel_assets_path($assets->$name, true);
   if (substr($uri, -3, 3) === 'css') {
-    wp_enqueue_style($name, $uri, ...$args);
+    wp_enqueue_style($name, $uri, $deps, $ver);
   } else {
-    wp_enqueue_script($name, $uri, ...$args);
+    wp_enqueue_script($name, $uri, $deps, $ver, $footer);
     wp_localize_script($name, $name . '_parcel', ['baseUrl' => parcel_assets_path('', true)]);
   }
 }
 
 add_action('wp_enqueue_scripts', function () {
   parcel_enqueue_resource('frontendCSS');
-  parcel_enqueue_resource('frontendJS', ['jquery'], null, true);
+  parcel_enqueue_resource('frontendJS', ['jquery']);
 });
 
 add_action('admin_enqueue_scripts', function () {
   parcel_enqueue_resource('backendCSS');
-  parcel_enqueue_resource('backendJS', ['jquery'], null, true);
+  parcel_enqueue_resource('backendJS', ['jquery']);
+});
+
+
+add_action('wp_default_scripts', function ($scripts) {
+  if (!is_admin() && ! empty($scripts->registered['jquery'])) {
+    $scripts->registered['jquery']->deps = array_diff(
+      $scripts->registered['jquery']->deps,
+      ['jquery-migrate']
+    );
+  }
 });
